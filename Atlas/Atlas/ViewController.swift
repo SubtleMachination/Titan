f0cba7ad659da6ed5c35ae4ab1b66b2b09348d68
SubtleMachination <arcanemx@gmail.com>
2017-02-18 14:07:47 -0700

    dumb logistical changes

--- Atlas/Atlas/ViewController.swift
+++ Atlas/Atlas/ViewController.swift
@@ -13,25 +13,20 @@ import GameplayKit
 class ViewController: NSViewController {
 
     @IBOutlet var skView: SKView!
+	
+	var scene:SKScene?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
+		
+		let skView = self.view as! SKView
+		scene = GameScene(size:(skView.frame.size))
+		
+		skView.showsFPS = true
+		skView.showsNodeCount = true
+		
+		scene!.scaleMode = .aspectFill
+		skView.presentScene(scene)
+	}
 }
 
