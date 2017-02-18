d6dbf2bb96c38cdbe872b3f6b00ba0c17122986d
Dusty Artifact <dusty@World-Domination-Device.local>
2017-02-18 12:05:03 -0700

    base project

---
+++ Atlas/Atlas/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Atlas
+//
+//  Created by Dusty Artifact on 2/18/17.
+//  Copyright © 2017 Overmind. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Atlas/Atlas/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Atlas
+//
+//  Created by Dusty Artifact on 2/18/17.
+//  Copyright © 2017 Overmind. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
